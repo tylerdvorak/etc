4bfda9b649c3a5a991c12826512f73292dd44dbd
= <=>
2023-07-11 08:24:52 -0500

    Added proper computer names

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -1,7 +1,7 @@
 # Edit this configuration file to define what should be installed on
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
-test
+
 { config, lib, pkgs, ... }:
 
 {
@@ -18,7 +18,7 @@ test
   boot.loader.efi.canTouchEfiVariables = true;
 
   # Set Hostname
-  networking.hostName = "nixos";
+  networking.hostName = "tonberry-nixos";
 
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
